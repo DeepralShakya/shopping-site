31b7f9e995d7c974c5d8ce71a07c7a4f65569211
DeepralShakya <shakyadeepral44@gmail.com>
2024-02-20 16:03:35 +0545

    adjustment

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <div className="bg-orange-300 text-white text-center py-4 pb-16 md:pb-4 mt-4">
-        Copyright © Sick <br /> All Rights Reserved 2023.
+        Copyright © Shop<br /> All Rights Reserved 2023.
       </div>
     );
   };
