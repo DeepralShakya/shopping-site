1e56e6986cbf5e465304937cb8ebe67a89a8715f
DeepralShakya <shakyadeepral44@gmail.com>
2024-02-20 11:53:40 +0545

    Header Top added

---
+++ components/HeaderTop.tsx
@@ -0,0 +1,50 @@
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa"
+
+const HeaderTop = () => {
+  return (
+    <div className="border-b border-black hidden sm:block">
+        <div className="container py-4">
+            <div className="flex justify-between items-center">
+                <div className="hidden lg:flex gap-1">
+                    <div className="header_top__icon_wrapper">
+                        <FaInstagram />
+                    </div>
+                    <div className="header_top__icon_wrapper">
+                        <FaGithub />
+                    </div>
+                    <div className="header_top__icon_wrapper">
+                        <FaLinkedin />
+                    </div>
+                </div>
+
+                <div className="text-gray-500 text-[12px]">
+                    <b>FREE SHIPPING</b> THIS WEEK ORDER OVER - RS.500
+                </div>
+
+                <div className="flex gap-4">
+                    <select
+                    className="text-gray-500 text-[12px] w-[70px]"
+                    name="currency"
+                    id="currency"
+                    >
+                    <option value="NPR">NPR</option>
+                    <option value="USD $">USD $</option>
+                    <option value="INR">INR</option>
+                    </select>
+
+                    <select
+                    className="text-gray-500 text-[12px] w-[80px]"
+                    name="language"
+                    id="language"
+                    >
+                    <option value="English">English</option>
+                    <option value="French">French</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeaderTop
